8a346d4c9299ad9aa46fd8f7d819ac9ee4510a2d
vicky274 <vimessi2@gmail.com>
2020-06-14 22:04:21 +0530

    Added lines

--- src/test/java/PageObject/LoginPageObject.java
+++ src/test/java/PageObject/LoginPageObject.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
-
+//page object
 public class LoginPageObject {
 
 	WebDriver driver;
