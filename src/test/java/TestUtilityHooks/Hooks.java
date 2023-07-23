dfd3062bcbfcb902d02f5fa513113ae221cf17ae
vimessi2 <vignesh.elangovan@telenetgroup.be>
2023-07-23 22:50:55 +0100

    Hooks

--- src/test/java/TestUtilityHooks/Hooks.java
+++ src/test/java/TestUtilityHooks/Hooks.java
@@ -8,7 +8,7 @@ import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
-import stepDefinitions.ApplicationTest;
+import stepDefinitions.ApplicationStepDefinition;
 
 import java.io.IOException;
 import java.time.Duration;
